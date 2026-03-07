247e3a8966669df795b1dae8e2afc759b38fff09
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-07 12:05:56 +0530

    Create Orders.js

---
+++ frontend/src/pages/Orders.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Orders(){
+  return(
+    <div>
+      <h1>Orders</h1>
+      <p>Place food orders from menu</p>
+    </div>
+  )
+}
+
+export default Orders;
