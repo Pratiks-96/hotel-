bc8fca057dc1b7da904d14c5eef4b81db450a617
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-07 12:05:22 +0530

    Create Home.js

---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to Hotel Rajeshwari</h1>
+      <p>Authentic food and great hospitality</p>
+    </div>
+  );
+}
+
+export default Home;
