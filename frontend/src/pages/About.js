e3af7386a7391ee10eb71714bb27be0513e566b7
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-07 12:35:07 +0530

    Create About.js

---
+++ frontend/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <h1>About Hotel Rajeshwari</h1>
+      <p>
+        Hotel Rajeshwari provides authentic food and excellent hospitality.
+      </p>
+    </div>
+  );
+}
+
+export default About;
