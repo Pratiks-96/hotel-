6aaf434cf035e338d734e12bf17af4f12e24668b
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-07 12:53:17 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,40 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Menu from "./pages/Menu";
 import Orders from "./pages/Orders";
 import Booking from "./pages/Booking";
-import Contact from "./pages/Contact";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
 
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router>
+
       <Navbar />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/menu" element={<Menu />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/booking" element={<Booking />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="container">
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/menu" element={<Menu />} />
+          <Route path="/orders" element={<Orders />} />
+          <Route path="/booking" element={<Booking />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+      </div>
 
       <Footer />
-    </BrowserRouter>
+
+    </Router>
   );
 }
 
