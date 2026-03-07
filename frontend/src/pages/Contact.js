5fd03c350578fde134ea6a858186843b53d0f4cb
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-07 12:34:53 +0530

    Create Contact.js

---
+++ frontend/src/pages/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact Hotel Rajeshwari</h1>
+     
+      <p>Email: info@hotelrajeshwari.com</p>
+    </div>
+  );
+}
+
+export default Contact;
