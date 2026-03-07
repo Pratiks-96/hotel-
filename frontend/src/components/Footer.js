4b4f437034e957c51627a6c13834cddbf7264cee
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-07 12:05:07 +0530

    Create Footer.js

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Hotel Rajeshwari</p>
+      <p>Created by Pratik Sawant</p>
+    </footer>
+  );
+}
+
+export default Footer;
