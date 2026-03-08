525a80f61bd2dd35a229910b62a07aae0368e5fe
Pratiks-96 <pratiksavant96@gmail.com>
2026-03-08 09:41:51 +0530

    Update app.py

--- services/enquiry-service/app.py
+++ services/enquiry-service/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from kubernetes import client, config
 
 app = Flask(__name__)
@@ -7,8 +7,9 @@ config.load_incluster_config()
 
 api = client.CustomObjectsApi()
 
+# CREATE ENQUIRY
 @app.route("/api/enquiry", methods=["POST"])
-def enquiry():
+def create_enquiry():
 
     data = request.json
 
@@ -33,6 +34,30 @@ def enquiry():
         body=body
     )
 
-    return {"status":"created"}
+    return {"status": "created"}
 
-app.run(host="0.0.0.0",port=5000)
+
+# GET ALL ENQUIRIES
+@app.route("/api/enquiries", methods=["GET"])
+def get_enquiries():
+
+    enquiries = api.list_namespaced_custom_object(
+        group="hotel.com",
+        version="v1",
+        namespace="default",
+        plural="enquiries"
+    )
+
+    results = []
+
+    for item in enquiries["items"]:
+        results.append({
+            "name": item["spec"]["name"],
+            "phone": item["spec"]["phone"],
+            "message": item["spec"]["message"]
+        })
+
+    return jsonify(results)
+
+
+app.run(host="0.0.0.0", port=5000)
